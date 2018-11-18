c111501b6a87da5ce0ebe621c2d918cc93d80bdb
manopatric <manoshivendrapatric@yahoo.com>
2018-11-18 17:17:35 +0530

    Index page change revert

--- WebClient/ClientApp/src/index.js
+++ WebClient/ClientApp/src/index.js
@@ -9,10 +9,3 @@ app.get('/',function(req,res){
 app.listen(3000,function(){
     console.log('Example app listening on port 3000!');
 })
-app.run(function($rootScope, $location){
-    $rootScope.$on('$routeChangeStart', function(event, next, current){
-      if ($location.path() == './tournament-index') {
-        $rootScope.hideit = true;
-      };
-    });
-  });
