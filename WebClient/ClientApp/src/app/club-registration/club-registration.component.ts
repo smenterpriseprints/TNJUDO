240a98e3443942366dcfb2f45c374c3b9abeeff7
manopatric <manoshivendrapatric@yahoo.com>
2018-11-24 13:32:37 +0530

    club-registration code modification

--- WebClient/ClientApp/src/app/club-registration/club-registration.component.ts
+++ WebClient/ClientApp/src/app/club-registration/club-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-club-registration',
@@ -11,5 +13,15 @@ export class ClubRegistrationComponent implements OnInit {
 
   ngOnInit() {
   }
+  isEditMode: boolean = true;
+  isValid: boolean = false;
+  public clubregistration: FormGroup = new FormGroup({});
 
+  onClubSubmit() {
+    debugger;
+    if (this.clubregistration.valid) {
+
+    }
+
+}
 }
