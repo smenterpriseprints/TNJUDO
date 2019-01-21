44e25265212a58afc1f28e6c9b7133c0e662f0d3
csemaheswaran <csemaheswaran@gmail.com>
2019-01-21 20:58:08 +0530

    IE fix

--- WebClient/ClientApp/src/app/app.component.ts
+++ WebClient/ClientApp/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import {Router} from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  constructor( router: Router){}
+  constructor(router: Router) { }
+
+  ngOnInit() {
+    this.loadScript("../assets/js/core.min.js");
+    this.loadScript("../assets/js/app.min.js");
+    this.loadScript("../assets/js/script.min.js");
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement>document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
